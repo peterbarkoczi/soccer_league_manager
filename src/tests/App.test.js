70c36bb05af35617fa797ccaeabe2975fa93ae9f
peterbarkoczi <barkopetya@gmail.com>
2020-06-09 11:31:14 +0200

    write a test to render the header

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,8 +3,18 @@ import { shallow } from "enzyme";
 import App from "../App";
 
 describe('App', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<App />);
+    })
+
     it('should render a <div />', function () {
-        const wrapper = shallow(<App />);
         expect(wrapper.find('div.App').length).toEqual(1);
     });
+
+    it('should render a <Header />', function () {
+        expect(wrapper.find('Header').length).toEqual(1);
+    });
+
 })
