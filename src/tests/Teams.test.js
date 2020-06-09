4db981887154be778f1f7e1527c1d9cb6d46fb6a
peterbarkoczi <barkopetya@gmail.com>
2020-06-09 18:25:53 +0200

    write test for <Teams />

--- src/tests/Teams.test.js
+++ src/tests/Teams.test.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { shallow } from "enzyme";
+import {shallow, mount} from "enzyme";
 import Teams from "../components/pages/Teams";
+import {TeamsProvider} from "../components/contexts/TeamsContext";
 
 describe('Teams', () => {
     let wrapper;
 
     beforeEach(() => {
-        wrapper = shallow(<Teams />);
+        wrapper = mount(
+            <TeamsProvider>
+                <Teams/>
+            </TeamsProvider>);
     });
 
     it('should render a <Teams /> component', function () {
-        expect(wrapper.find('div.teams').length).toEqual(1);
+        expect(wrapper.find('.team').length).toEqual(11);
     });
 })
