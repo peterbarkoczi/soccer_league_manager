c413e1fd89bbf69cf0a79f23bfb1b1accc1be0a4
peterbarkoczi <barkopetya@gmail.com>
2020-06-09 18:23:57 +0200

    created Teams component

--- src/components/pages/Teams.js
+++ src/components/pages/Teams.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import {TeamsContext} from "../contexts/TeamsContext";
 
 function Teams() {
+    const [teams, setTeams] = useContext(TeamsContext);
+
     return (
-        <div className="teams"></div>
+        <div className="teams">
+            <h1 className="teamsTitle">
+                Teams
+            </h1>
+            <ul className="teamsList">
+                {teams.map(team => (<li className="team" key={team.id}>{team.team}</li> ))}
+            </ul>
+        </div>
     );
 }
 
