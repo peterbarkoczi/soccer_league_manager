8d0148db42d11e37815895c31ac5454c77942b4f
peterbarkoczi <barkopetya@gmail.com>
2020-06-10 17:54:43 +0200

    add more tests to Teams.test.js

--- src/tests/Teams.test.js
+++ src/tests/Teams.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {shallow, mount} from "enzyme";
+import {mount} from "enzyme";
 import Teams from "../components/pages/Teams";
 import {TeamsProvider} from "../components/contexts/TeamsContext";
 
@@ -13,7 +13,17 @@ describe('Teams', () => {
             </TeamsProvider>);
     });
 
+    it('should render a page title', function () {
+        expect(wrapper.find('#teamsTitle').length).toEqual(1);
+    });
+
     it('should render a <Teams /> component', function () {
         expect(wrapper.find('.team').length).toEqual(11);
+    });
+
+    it('should render <AddTeam /> component', function () {
+        expect(wrapper.find('#addNewTeam').length).toEqual(0);
+        wrapper.find('#addNewTeamButton').simulate('click');
+        expect(wrapper.find('#addNewTeam').length).toEqual(1);
     });
 })
