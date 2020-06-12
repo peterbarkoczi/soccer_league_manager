1e2affdf3f41fdcb89cc1a413af0f1a662f192c1
peterbarkoczi <barkopetya@gmail.com>
2020-06-12 09:54:53 +0200

    make style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,57 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import styled from "styled-components";
+import background from "../red-soccer-bg2.jpg"
 
 const HeaderStyle = styled.div`
-   text-align: center;
-   padding: 2%;
-   margin: 2%;
-   border: dotted aquamarine;
    display: flex;
-   justify-content: space-between;
    
-   //a {
-   //   text-decoration: none;
-   //}
-   
-   .logo h4 {
-    color: green;
-    
-    &:hover {
-       color: blue;
-    }
+   .header {
+     width: 100%;
+     background-image: url(${background});
    }
    
-   .menu h4 {
-      color: red;
+   .title {
+      position: relative;
+   }
+   
+   .title h2 {
+      color: whitesmoke;
+      font-size: 5em;
+      margin-top: 0;
+      margin-bottom: 2%;
+      padding-left: 1%;
+   }
+   
+   .menu {
+     position: absolute;
+   }
+   
+   .menu h3 {
+      color: black;
       margin: 1%;
+      border: 1px solid red;
+      display: inline;
+      
    }
+   
+   .login {
+      margin: 1%;
+      float: right;
+   }
+   
+   .login button {
+    display: inline-block;
+    position: relative;
+    margin: 10px;
+    padding: 0 20px;
+    text-align: center;
+    text-decoration: none;
+    font: bold 12px/25px Arial, sans-serif;
+    color: #515151;
+    background: #d3d3d3;
+    border-radius: 30px;
+}
     
 `;
 
@@ -33,19 +59,19 @@ function Header() {
     return (
         <HeaderStyle>
             <div className="header">
-                <div className="logo">
-                    <Link to="/">
-                        <h4>Soccer League Manager</h4>
-                    </Link>
-                </div>
-                <div className="menu">
-                    <Link to="/bajnoksag"><h4>Bajnokság</h4></Link>
-                    <Link to="/kupak"><h4>Kupák</h4></Link>
-                    <Link to="/csapatok"><h4>Csapatok</h4></Link>
-                </div>
                 <div className="login">
                     <button>Login</button>
                     <button>Register</button>
+                </div>
+                <div className="title">
+                    <Link to="/">
+                        <h2>Soccer League Manager</h2>
+                    </Link>
+                </div>
+                <div className="menu">
+                    <Link to="/bajnoksag"><h3>Bajnokság</h3></Link>
+                    <Link to="/kupak"><h3>Kupák</h3></Link>
+                    <Link to="/csapatok"><h3>Csapatok</h3></Link>
                 </div>
             </div>
         </HeaderStyle>
