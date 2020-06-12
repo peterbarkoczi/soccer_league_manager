65561016fb789f3c9803a71bf585b95e1226dd7f
peterbarkoczi <barkopetya@gmail.com>
2020-06-12 09:54:30 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,24 @@ import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Header from "./components/Header";
 import Teams from "./components/pages/Teams";
+import Leagues from "./components/pages/Leagues";
 import {TeamsProvider} from "./components/contexts/TeamsContext";
+import {LeagueProvider} from "./components/contexts/LeagueContext";
+import SubLeagues from "./components/pages/SubLeagues";
 
 function App() {
     return (
         <TeamsProvider>
-            <Router>
-                <div className="App">
-                    <Header/>
-                    <Route exact path="/csapatok" component={Teams}/>
-                </div>
-            </Router>
+            <LeagueProvider>
+                <Router>
+                    <div className="App">
+                        <Header/>
+                        <Route exact path="/csapatok" component={Teams}/>
+                        <Route exact path="/bajnoksag" component={Leagues}/>
+                        <Route exact path="/bajnoksag/:leagueName" component={SubLeagues}/>
+                    </div>
+                </Router>
+            </LeagueProvider>
         </TeamsProvider>
     );
 }
