2a1adef75b5a062259d54d527186719bf580b458
peterbarkoczi <barkopetya@gmail.com>
2020-06-19 10:40:54 +0200

    add useEffect hook

--- src/components/pages/SubLeagues.js
+++ src/components/pages/SubLeagues.js
@@ -1,24 +1,41 @@
-import React, {useState, useContext} from "react";
+import React, {useState, useContext, useEffect} from "react";
 import {LeagueContext} from "../contexts/LeagueContext";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const SubLeagues = (props) => {
     const {subLeagues, setSubLeagues} = useContext(LeagueContext);
     const [isLoading, setIsLoading] = useState(false);
 
-    return (
-        <div className="subLeagues">
-            <h1>Bajnokság</h1>
-            <ul className="list" id="subLeaguesList">
-                {subLeagues.map(subLeague => (
-                    <li className="team" key={subLeague.name}>
-                        <Link to={`/${subLeague.name.split(" ").join("")}`}>{subLeague.name}</Link>
-                    </li>))
-                }
-            </ul>
-        </div>
-    )
+    useEffect(() => {
+        console.log(props)
+        setIsLoading(true);
+        axios.get(`http://localhost:3000/subLeagues?leagueId=${props.location.id}`)
+            .then((response) => setSubLeagues(response.data))
+            .then(() => setIsLoading(false));
+    }, [props, setSubLeagues]);
 
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    } else {
+        return (
+            <div className="subLeagues">
+                <div className="title">
+                    <h1>Bajnokság</h1>
+                </div>
+                <ul className="list" id="subLeaguesList">
+                    {subLeagues.map(subLeague => (
+                        <li className="team" key={subLeague.name}>
+                            <Link to={{
+                                pathname: `${props.location.pathname}/${subLeague.name.split(" ").join("")}`,
+                                subLeagueName: subLeague.name
+                            }}>{subLeague.name}</Link>
+                        </li>))
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default SubLeagues;
