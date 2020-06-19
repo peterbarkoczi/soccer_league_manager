9d49aafb2093d75dc912270d1b4413a031e4d000
peterbarkoczi <barkopetya@gmail.com>
2020-06-19 10:41:28 +0200

    add useEffect hook to Leagues.js

--- src/components/pages/Leagues.js
+++ src/components/pages/Leagues.js
@@ -1,25 +1,43 @@
-import React, {useState, useContext} from "react";
+import React, {useState, useContext, useEffect} from "react";
 import {LeagueContext} from "../contexts/LeagueContext";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 function Leagues() {
     const {leagues, setLeagues} = useContext(LeagueContext);
     const [isLoading, setIsLoading] = useState(false);
     const [showLeaguesDiv, setShowLeaguesDiv] = useState(true);
 
-    return (
+    useEffect(() => {
+        setIsLoading(true);
+        axios.get("http://localhost:3000/leagues")
+            .then((response) => setLeagues(response.data))
+            .then(() => setIsLoading(false));
+    }, [setLeagues]);
+
+    const LeaguesDiv = () => (
         <div className="leagues">
-            <h1>Liga</h1>
+            <h1>Liga:</h1>
             <ul className="list" id="leaguesList">
                 {leagues.map(league => (
                     <li className="team" key={league.name}>
-                        <Link to={`bajnoksag/${league.name.split(" ").join("")}`}>{league.name}</Link>
+                        <Link to={{
+                            pathname: `liga/${league.name.split(" ").join("")}`,
+                            id: league.id
+                        }} onClick={() => setShowLeaguesDiv(false)}>{league.name}</Link>
                     </li>))
                 }
             </ul>
         </div>
-
     )
+
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    } else {
+        return (
+            showLeaguesDiv ? <LeaguesDiv/> : null
+        )
+    }
 }
 
 export default Leagues;
