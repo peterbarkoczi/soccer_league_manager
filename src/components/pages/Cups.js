30e1487160c8653ffe3ae66135d6ac79605f9fcc
peterbarkoczi <barkopetya@gmail.com>
2020-06-21 15:08:59 +0200

    add title for Cups

--- src/components/pages/Cups.js
+++ src/components/pages/Cups.js
@@ -3,6 +3,7 @@ import React from "react";
 const Cups = () => {
     return (
         <div className="cups">
+            <h1>Kupa</h1>
             <button>Új kupa létrehozása</button>
         </div>
     )
