a57038a13ce0d7aa2837fb87b19706f05393d26d
peterbarkoczi <barkopetya@gmail.com>
2020-06-21 15:14:08 +0200

    create UseEffect hook

--- src/components/util/AddTeam.js
+++ src/components/util/AddTeam.js
@@ -1,10 +1,20 @@
-import React, { useState, useContext } from "react";
+import React, {useState, useContext, useEffect} from "react";
 import {TeamsContext} from "../contexts/TeamsContext";
+import axios from "axios";
 
 function AddTeam() {
     const [teamName, setTeamName] = useState("");
     const [id, setId] = useState("");
+    const [isAdded, setIsAdded] = useState(false);
     const {teams, setTeams} = useContext(TeamsContext);
+
+    useEffect(() => {
+        if (isAdded) {
+            axios.post('http://localhost:3000/teams', {teamName: teamName, id: parseInt(id), leagueId: parseInt(localStorage.getItem("leagueId"))})
+                .then(response => console.log("team added" + response))
+                .then(() => setIsAdded(false));
+        }
+    }, [isAdded])
 
     const updateTeamName = e => {
         setTeamName(e.target.value);
@@ -16,13 +26,14 @@ function AddTeam() {
 
     function addTeam(e) {
         e.preventDefault();
-        setTeams(prevTeams => [...teams, {teamName: teamName, id: id }]);
+        setTeams(prevTeams => [...teams, {teamName: teamName, id: parseInt(id), leagueId: parseInt(localStorage.getItem("leagueId"))}]);
+        setIsAdded(true);
     }
 
     return (
         <form id="addNewTeam" onSubmit={addTeam}>
-            <input type="text" name="teamName" value={teamName} onChange={updateTeamName} />
-            <input type="number" name="id" value={id} onChange={updateId} />
+            <input type="text" name="teamName" value={teamName} onChange={updateTeamName}/>
+            <input type="number" name="id" value={id} onChange={updateId}/>
             <button>Submit</button>
         </form>
     )
