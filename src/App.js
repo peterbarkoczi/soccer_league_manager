f7c4688a8ff3e9984e701606f70896ef907df91f
peterbarkoczi <barkopetya@gmail.com>
2020-06-23 13:53:48 +0200

    import bootstrap

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./components/Header";
 import Teams from "./components/pages/Teams";
