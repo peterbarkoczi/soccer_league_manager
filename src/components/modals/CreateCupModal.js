0cde457ff241477459b721654f8c3acc096a2db8
peterbarkoczi <barkopetya@gmail.com>
2020-06-25 21:10:04 +0200

    create CreateCupModal.js

---
+++ src/components/modals/CreateCupModal.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+import {Button, Modal} from "react-bootstrap";
+
+const CreateCupModal = () => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    return (
+        <>
+            <Button variant="danger" onClick={handleShow}>
+                Új kupa létrehozása
+            </Button>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default CreateCupModal;
