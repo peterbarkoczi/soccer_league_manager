285f1067176a422fd11c4d4ec57b124d71e5d156
peterbarkoczi <barkopetya@gmail.com>
2020-06-25 21:12:39 +0200

    add eseEffect to Cups for fetch cups of selected league

--- src/components/pages/Cups.js
+++ src/components/pages/Cups.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import {CupsContext} from "../contexts/CupsContext";
+import CreateCupModal from "../modals/CreateCupModal";
+import {ListGroup} from "react-bootstrap";
+import {Link} from "react-router-dom";
+import axios from "axios";
 
 const Cups = () => {
+    const {cups, setCups} = useContext(CupsContext);
+
+    useEffect(() => {
+        axios.get(`http://localhost:3000/cups?leagueId=${localStorage.getItem("leagueId")}`)
+            .then((response) => setCups(response.data));
+    }, [setCups]);
+
     return (
         <div className="cups">
-            <h1>Kupa</h1>
-            <button>Új kupa létrehozása</button>
+            <h1 id="cupsTitle">Kupák</h1>
+            <CreateCupModal/>
+            <ListGroup id="cupsList">
+                {cups.map(cup => (
+                    <ListGroup.Item key={cup.name}>
+                        <Link to={{
+                            pathname: `/${localStorage.getItem("path")}/kupak/${cup.name.split(" ").join("")}`,
+                            cup: cup
+                        }}>{cup.name}</Link>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>
         </div>
     )
 }
