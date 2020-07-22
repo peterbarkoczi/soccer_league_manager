6fd5b793a3772e628c707d17fc73dccf3be98d0f
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:38:16 +0200

    refactor imports and routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from "./components/Header";
 import Teams from "./components/pages/Teams";
+import Location from "./components/pages/Location";
+import {DataPackProvider} from "./components/contexts/DataPackContext";
 import Leagues from "./components/pages/Leagues";
-import {TeamsProvider} from "./components/contexts/TeamsContext";
-import {LeagueProvider} from "./components/contexts/LeagueContext";
-import SubLeagues from "./components/pages/SubLeagues";
 import LeagueDetails from "./components/pages/LeagueDetails";
 import Cups from "./components/pages/Cups";
 import CupDetails from "./components/pages/CupDetails"
@@ -15,23 +14,21 @@ import {CupsProvider} from "./components/contexts/CupsContext";
 
 function App() {
     return (
-        <TeamsProvider>
-            <LeagueProvider>
+            <DataPackProvider>
                 <CupsProvider>
                     <Router>
                         <div className="App">
                             <Header/>
-                            <Leagues/>
+                            <Route exact path="/" component={Location}/>
                             <Route exact path="/liga/:league/csapatok" component={Teams}/>
-                            <Route exact path="/liga/:league/bajnoksag" component={SubLeagues}/>
+                            <Route exact path="/liga/:league/bajnoksag" component={Leagues}/>
                             <Route exact path="/liga/:league/bajnoksag/:subLeague" component={LeagueDetails}/>
                             <Route exact path="/liga/:league/kupak" component={Cups}/>
                             <Route exact path="/liga/:league/kupak/:cup" component={CupDetails}/>
                         </div>
                     </Router>
                 </CupsProvider>
-            </LeagueProvider>
-        </TeamsProvider>
+            </DataPackProvider>
     );
 }
 
