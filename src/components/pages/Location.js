4ba50ab544035d09973259cb229f5a083ec9c5e0
peterbarkoczi <barkopetya@gmail.com>
2020-07-22 16:33:53 +0200

    rename Leagues to Location

---
+++ src/components/pages/Location.js
@@ -0,0 +1,49 @@
+import React, {useContext, useEffect, useState} from "react";
+import {DataPackContext} from "../contexts/DataPackContext";
+import {Link} from "react-router-dom";
+import {ListGroup} from "react-bootstrap";
+import AddLocationModal from "../modals/AddLocationModal";
+
+function Location() {
+    const {dataPack, setIsSelected, showLocationDiv, setShowLocationDiv, isLoading} = useContext(DataPackContext);
+    const [location, setLocation] = useState([]);
+
+    useEffect(() => {
+        setLocation(dataPack);
+    }, [dataPack])
+
+    const LocationDiv = () => (
+        <div className="locations">
+            <div id="addLocation">
+                <AddLocationModal/>
+            </div>
+            <h1 id="locationName">Helyszín:</h1>
+            <ListGroup className="list" id="locationList">
+                {location.map(location => (
+                    <ListGroup.Item className="location" key={location.name}>
+                        <Link to={{
+                            pathname: `liga/${location.name.split(" ").join("")}/bajnoksag`,
+                            locationId: location.id
+                        }} onClick={() => {
+                            setShowLocationDiv(false);
+                            setIsSelected(true);
+                            localStorage.setItem("locationId", location.id);
+                            localStorage.setItem("path", `liga/${location.name.split(" ").join("")}`);
+                            localStorage.setItem("location", location.name)
+                        }}>{location.name}</Link>
+                    </ListGroup.Item>))
+                }
+            </ListGroup>
+        </div>
+    )
+
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    } else {
+        return (
+            showLocationDiv ? <LocationDiv/> : null
+        )
+    }
+}
+
+export default Location;
