30e9c8c1780afa9b2de475a5f0d9b08d54bf30b0
peterbarkoczi <barkopetya@gmail.com>
2020-08-03 13:21:11 +0200

    change CreateCupModal folder

--- src/components/pages/cup/Cups.js
+++ src/components/pages/cup/Cups.js
@@ -1,5 +1,5 @@
 import React, {useContext, useEffect, useState} from "react";
-import CreateCupModal from "../modals/CreateCupModal";
+import CreateCupModal from "../../modals/CreateCupModal";
 import {ListGroup} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import {DataPackContext} from "../contexts/DataPackContext";
