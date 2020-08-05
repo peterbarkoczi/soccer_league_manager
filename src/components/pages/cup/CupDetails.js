591f58a45602a253ce6ff7ed0f8b6c4061fd39e2
peterbarkoczi <barkopetya@gmail.com>
2020-08-05 09:42:32 +0200

    created different requests for all match types

--- src/components/pages/cup/CupDetails.js
+++ src/components/pages/cup/CupDetails.js
@@ -1,17 +1,89 @@
-import React, {useContext, useEffect} from "react";
-import QualifierForCup from "../../util/QualifierForCup";
+import React, {useContext, useEffect, useState} from "react";
+import DisplayMatches from "../../util/DisplayMatches";
 import {CupContext} from "../../contexts/CupContext";
+import axios from "axios";
 
 const CupDetails = () => {
 
-    const {cup, matches, setMatches, isLoading} = useContext(CupContext);
+    const {
+        cup, cupId,
+        matchIsFinished,
+        isLoading, setIsLoading,
+        scoreIsAdded, setScoreIsAdded
+    } = useContext(CupContext);
+
+    const [qualifierMatches, setQualifierMatches] = useState([]);
+    const [sfIsReady, setSfIsReady] = useState(false)
+    const [semiFinalMatches, setSemiFinalMatches] = useState([]);
+    const [finalIsReady, setFinalIsReady] = useState(false)
+    const [finalMatches, setFinalMatches] = useState([]);
+
+    let finalTitles = ["Bronz meccs", "Döntő"];
+    let titleIndex = 0;
+
     let index = 1;
 
     useEffect(() => {
-        if (isLoading) {
-            setMatches(cup.matches);
+        setIsLoading(true);
+        if (cupId !== "") {
+            axios.get(`http://localhost:8080/match/get_qualifiers?cupId=${cupId}&matchType=q`)
+                .then((response) => setQualifierMatches(response.data))
+                .then(() => setIsLoading(false))
+                .then(() => setScoreIsAdded(false));
         }
-    }, [cup, isLoading, setMatches])
+    }, [cupId, scoreIsAdded, matchIsFinished]);
+
+    useEffect(() => {
+        if (qualifierMatches.length !== 0) {
+            let counter = 0;
+            for (let match of qualifierMatches) {
+                if (match.finished === true) {
+                    counter++;
+                }
+            }
+            if (counter === qualifierMatches.length && semiFinalMatches.length === 0) {
+                axios.get(`http://localhost:8080/match/create_semi_finals?cupId=${cupId}&matchType=sf`)
+                    .then((response) => setSemiFinalMatches(response.data))
+                    .then(() => setSfIsReady(true));
+            }
+        }
+    }, [qualifierMatches])
+
+    useEffect(() => {
+        setIsLoading(true);
+        if (cupId !== "") {
+            axios.get(`http://localhost:8080/match/get_semifinals?cupId=${cupId}&matchType=sf`)
+                .then((response) => setSemiFinalMatches(response.data))
+                .then(() => setIsLoading(false))
+                .then(() => setScoreIsAdded(false));
+        }
+    }, [cupId, scoreIsAdded, matchIsFinished, sfIsReady])
+
+    useEffect(() => {
+        if (semiFinalMatches.length !== 0) {
+            let counter = 0;
+            for (let match of semiFinalMatches) {
+                if (match.finished === true) {
+                    counter++;
+                }
+            }
+            if (counter === semiFinalMatches.length && finalMatches.length === 0) {
+                axios.get(`http://localhost:8080/match/create_semi_finals?cupId=${cupId}&matchType=f`)
+                    .then((response) => setFinalMatches(response.data))
+                    .then(() => setFinalIsReady(true));
+            }
+        }
+    }, [qualifierMatches])
+
+    useEffect(() => {
+        setIsLoading(true);
+        if (cupId !== "") {
+            axios.get(`http://localhost:8080/match/get_semifinals?cupId=${cupId}&matchType=f`)
+                .then((response) => setFinalMatches(response.data))
+                .then(() => setIsLoading(false))
+                .then(() => setScoreIsAdded(false));
+        }
+    }, [cupId, scoreIsAdded, matchIsFinished, finalIsReady])
 
     if (isLoading) {
         return (<h1>Loading...</h1>)
@@ -19,9 +91,23 @@ const CupDetails = () => {
         return (
             <div>
                 <h1 id="cupName">{cup.name}</h1>
-                {matches.map(match => (
-                    <QualifierForCup key={"match" + match.id} match={match} index={index++}/>
+                <h3 className="matchTypeTitle">Selejtező</h3>
+                {qualifierMatches.map(match => (
+                    <DisplayMatches key={match.id} match={match} index={index++} matchType={"Selejtező"}/>
                 ))}
+
+                <h3 className="matchTypeTitle">Elődöntő</h3>
+                {semiFinalMatches ?
+                    semiFinalMatches.map(match => (
+                        <DisplayMatches key={match.id} match={match} index={index++} matchType={"Elődöntő"}/>
+                    )) : null}
+
+                <h3 className="matchTypeTitle">Döntő</h3>
+                {finalMatches ?
+                    finalMatches.map(match => (
+                        <DisplayMatches key={match.id} match={match} index={index++} matchType={finalTitles[titleIndex++]}/>
+                    )) : null}
+
             </div>
         );
     }
