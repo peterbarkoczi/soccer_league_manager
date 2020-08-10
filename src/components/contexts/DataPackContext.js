62dab20aaf978204dc8339ba8b22003c3a84c66d
peterbarkoczi <barkopetya@gmail.com>
2020-08-10 14:43:53 +0200

    refactor

--- src/components/contexts/DataPackContext.js
+++ src/components/contexts/DataPackContext.js
@@ -8,20 +8,24 @@ export const DataPackProvider = props => {
     const [isLoading, setIsLoading] = useState(false);
     const [isSelected, setIsSelected] = useState(false);
     const [showLocationDiv, setShowLocationDiv] = useState(true);
+    const [locationIsDeleted, setLocationIsDeleted] = useState(false);
+    const [teamIsDeleted, setTeamIsDeleted] = useState(false);
 
     useEffect(() => {
         setIsLoading(true);
         axios.get("http://localhost:8080/location/list")
             .then((response) => setDataPack(response.data))
             .then(() => setIsLoading(false));
-    }, [setDataPack]);
+    }, [setDataPack, locationIsDeleted, teamIsDeleted]);
 
     return (
         <DataPackContext.Provider value={{
             dataPack,
             isLoading, setIsLoading,
             isSelected, setIsSelected,
-            showLocationDiv, setShowLocationDiv}}>
+            showLocationDiv, setShowLocationDiv,
+            locationIsDeleted, setLocationIsDeleted,
+            teamIsDeleted, setTeamIsDeleted}}>
             {props.children}
         </DataPackContext.Provider>
     )
