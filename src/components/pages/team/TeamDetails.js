e0d51cd143a3f7a51026a69c1bf1ded366bd9c46
peterbarkoczi <barkopetya@gmail.com>
2020-08-26 10:04:27 +0200

    delete style for table

--- src/components/pages/team/TeamDetails.js
+++ src/components/pages/team/TeamDetails.js
@@ -1,35 +1,10 @@
 import React, {useContext, useEffect, useState} from "react";
 import {Table} from "react-bootstrap";
 import axios from "axios";
-import styled from "styled-components";
 import AddPlayerModal from "../../modals/AddPlayerModal";
 import {DataPackContext} from "../../contexts/DataPackContext";
 
-const TeamDetails = (props) => {
-
-    const TableStyle = styled.div`
-
-       .playerIndexCell {
-          width: 5%;
-       }
-       
-       .playerNumberCell {
-          width: 5%;
-       }
-       
-       .playerNameCell{
-          width: 50%;
-       }
-       
-       .playerGoalsCell{
-          width: 20%;
-       }
-       
-       th {
-          text-align: center;
-       }
-
-    `;
+const TeamDetails = () => {
 
     const {playerAdded, setPlayerAdded, setIsSelected} = useContext(DataPackContext);
     const [playerList, setPlayerList] = useState([]);
@@ -49,39 +24,36 @@ const TeamDetails = (props) => {
         return (<h1>Loading...</h1>)
     } else {
         return (
-            <div id={playerList}>
+            <div id="playerList">
                 <h1>{localStorage.getItem("teamName")}</h1>
                 <AddPlayerModal teamId={localStorage.getItem("teamId")}/>
-                <TableStyle>
-                    <Table striped bordered hover size="sm">
-                        <colgroup>
-                            <col className="playerIndexCell"/>
-                            <col className="playerNumberCell"/>
-                            <col className="playerNameCell"/>
-                            <col className="playerGoalsCell"/>
-                        </colgroup>
-                        <thead>
-                            <tr>
-                                <th></th>
-                                <th>Mezszám</th>
-                                <th>Név</th>
-                                <th>Gólok</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {playerList.map(player => (
-                            <tr key={player.id}>
-                                <td>{index++}</td>
-                                <td>{player.number}</td>
-                                <td id="playerName">{player.name}</td>
-                                <td>{player.goals}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </Table>
-                </TableStyle>
+                <Table id="playersTable" striped bordered hover size="sm">
+                    <colgroup>
+                        <col className="playerIndexCell"/>
+                        <col className="playerNumberCell"/>
+                        <col className="playerNameCell"/>
+                        <col className="playerGoalsCell"/>
+                    </colgroup>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Mezszám</th>
+                        <th>Név</th>
+                        <th>Gólok</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {playerList.map(player => (
+                        <tr key={player.id}>
+                            <td>{index++}</td>
+                            <td>{player.number}</td>
+                            <td id="playerName">{player.name}</td>
+                            <td>{player.goals}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </Table>
             </div>
-
         );
     }
 }
