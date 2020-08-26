2bd5a2d6f5a15254027d4519af51bedd729db1dc
peterbarkoczi <barkopetya@gmail.com>
2020-08-26 13:49:59 +0200

    created LeagueMatches component

---
+++ src/components/pages/league/LeagueMatches.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import DisplayMatches from "../../util/DisplayMatches";
+
+const LeagueMatches = () => {
+
+
+
+}
+
+export default LeagueMatches;
