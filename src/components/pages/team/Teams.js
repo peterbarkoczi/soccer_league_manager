38147c50986c10f4a0789f2d680335dcf7fafe38
peterbarkoczi <barkopetya@gmail.com>
2020-08-27 16:46:41 +0200

    created custom hook to delete process

--- src/components/pages/team/Teams.js
+++ src/components/pages/team/Teams.js
@@ -1,16 +1,32 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {lazy, Suspense, useContext, useEffect, useState} from "react";
 import {Link} from "react-router-dom";
 import AddTeamModal from "../../modals/AddTeamModal";
-import {ListGroup} from "react-bootstrap";
+import {Button, ListGroup} from "react-bootstrap";
 import axios from "axios";
 import DeleteModal from "../../modals/DeleteModal";
 import {DataPackContext} from "../../contexts/DataPackContext";
+
+function usePrefetch(factory) {
+    const [component, setComponent] = useState(null);
+
+    useEffect(() => {
+        factory();
+        const comp = lazy(factory);
+        setComponent(comp);
+    }, [factory]);
+    return component;
+}
+
+const importModal = () => import("../../modals/DeleteModal");
 
 function Teams() {
     const [teams, setTeams] = useState([]);
     const [isAdded, setIsAdded] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
-    const {teamIsDeleted} = useContext(DataPackContext);
+    const {teamIsDeleted, setTeamIsDeleted, isShown, setIsShown} = useContext(DataPackContext);
+
+    const [selectedId, setSelectedId] = useState(0);
+    const DeleteModal = usePrefetch(importModal);
 
     useEffect(() => {
         localStorage.removeItem("teamId");
@@ -18,6 +34,7 @@ function Teams() {
         setIsLoading(true);
         axios.get(`http://localhost:8080/teams?id=${localStorage.getItem("locationId")}`)
             .then(response => setTeams(response.data))
+            .then(() => setTeamIsDeleted(false))
             .then(() => setIsLoading(false))
     }, [isAdded, teamIsDeleted]);
 
@@ -41,7 +58,15 @@ function Teams() {
                                     localStorage.setItem("teamId", team.id);
                                     localStorage.setItem("teamName", team.name)
                                 }}>{(i+1) + ". " + team.name}</Link>
-                            {'   '}<DeleteModal id={team.id} url="teams"/>
+                            {'   '}
+                            <Button variant="warning" onClick={() => {
+                                setIsShown(true);
+                                setSelectedId(team.id)}}>
+                                Törlés
+                            </Button>
+                            <Suspense fallback={<h1>Loading...</h1>}>
+                                {isShown && selectedId === team.id && <DeleteModal id={selectedId} name={team.name} url="teams"/>}
+                            </Suspense>
                         </ListGroup.Item>)
                     )}
                 </ListGroup>
