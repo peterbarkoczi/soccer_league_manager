8c7766982cef7ff2f6acda330993db5d40465588
peterbarkoczi <barkopetya@gmail.com>
2020-09-02 17:45:07 +0200

    added fields to check qualifiers and semifinals match status

--- src/components/contexts/MatchContext.js
+++ src/components/contexts/MatchContext.js
@@ -5,15 +5,19 @@ export const MatchContext = createContext();
 export const MatchProvider = props => {
 
     const [qualifierMatches, setQualifierMatches] = useState([]);
+    const [qualifiersFinished, setQualifiersFinished] = useState(false);
     const [sfIsReady, setSfIsReady] = useState(false)
     const [semiFinalMatches, setSemiFinalMatches] = useState([]);
+    const [semiFinalsFinished, setSemiFinalsFinished] = useState(false);
     const [finalIsReady, setFinalIsReady] = useState(false)
     const [finalMatches, setFinalMatches] = useState([]);
 
     return (
         <MatchContext.Provider value={{
             qualifierMatches, setQualifierMatches,
+            qualifiersFinished, setQualifiersFinished,
             semiFinalMatches, setSemiFinalMatches,
+            semiFinalsFinished, setSemiFinalsFinished,
             finalMatches, setFinalMatches,
             sfIsReady, setSfIsReady,
             finalIsReady, setFinalIsReady
