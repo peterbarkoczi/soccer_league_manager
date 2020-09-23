d59683bb1902015f342716a12329de6226105bc7
peterbarkoczi <barkopetya@gmail.com>
2020-09-23 14:37:11 +0200

    created league matches component to show matches and table

--- src/components/pages/league/LeagueMatches.js
+++ src/components/pages/league/LeagueMatches.js
@@ -1,11 +1,43 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useContext} from "react";
 import axios from "axios";
 import DisplayMatches from "../../util/DisplayMatches";
+import {MatchContext} from "../../contexts/MatchContext";
 
 const LeagueMatches = () => {
 
+    const {leagueMatchFinished} = useContext(MatchContext);
+    const [matches, setMatches] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
+    useEffect(() => {
+        const source = axios.CancelToken.source();
 
+        setIsLoading(true);
+        axios.get(`http://localhost:8080/match/get_league_matches?leagueId=${localStorage.getItem("leagueId")}`,
+            {cancelToken: source.token})
+            .then((response) => setMatches(response.data))
+            .then(() => setIsLoading(false));
+
+        return () => {
+            source.cancel("Component got unmounted");
+        }
+    }, [leagueMatchFinished])
+
+    const setMatchType = (matchType) => (
+        `${localStorage.getItem("leagueName")} - ${Number(matchType.slice(-2))}. foruló`
+    )
+
+    if (isLoading) {
+        return (<h1>Loading...</h1>)
+    } else {
+        return (
+            <div>
+                {matches.map((match, index) => (
+                    <DisplayMatches key={match.id} match={match} index={++index} matchType={setMatchType(match.matchType)} />
+                ))}
+            </div>
+        )
+    }
 }
 
 export default LeagueMatches;
