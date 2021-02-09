17f922a701f474dc17b67e637cebe04008fa8103
peterbarkoczi <barkopetya@gmail.com>
2021-02-09 11:51:25 +0100

    refactor: can be able to edit contacts; refresh the contact page when the edit has been finished

--- src/components/pages/contact/Contact.js
+++ src/components/pages/contact/Contact.js
@@ -1,7 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import axios from "axios";
 import styled from "styled-components";
 import {useParams} from "react-router-dom";
+import EditContactsModal from "../../modals/EditContactsModal";
+import {DataPackContext} from "../../contexts/DataPackContext"
+
+import LocationOnTwoToneIcon from '@material-ui/icons/LocationOnTwoTone';
+import PersonIcon from '@material-ui/icons/Person';
+import PhoneAndroidTwoToneIcon from '@material-ui/icons/PhoneAndroidTwoTone';
+import MailIcon from '@material-ui/icons/Mail';
+import {hasRole} from "../../util/Auth";
 
 const ContactStyle = styled.div`
 
@@ -21,6 +29,11 @@ const ContactStyle = styled.div`
     margin-left: 1.2%;
     margin-bottom: 1%;
   }
+
+  .icon {
+    font-size: 1.3em;
+  }
+
 `
 
 const Contact = () => {
@@ -29,6 +42,8 @@ const Contact = () => {
     const [actualLocation, setActualLocation] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
 
+    const {refresh, setRefresh} = useContext(DataPackContext)
+
     useEffect(() => {
         setIsLoading(true);
         axios.get(`${process.env.REACT_APP_API_URL}/location/get_location_contact`,
@@ -36,13 +51,14 @@ const Contact = () => {
             .then((response) => {
                 setActualLocation(response.data);
                 setIsLoading(false);
+                setRefresh(false);
             })
 
         return () => {
             console.log("Contact unmounted");
         }
 
-    }, [])
+    }, [refresh])
 
     if (isLoading) {
         return (<h1>Loading...</h1>)
@@ -52,16 +68,19 @@ const Contact = () => {
                 <div className="contentList">
                     <h1>Kapcsolat</h1>
                     <div id="contactDetails">
-                        <h3>Pálya címe:{" "}
+                        <h3><LocationOnTwoToneIcon className={"icon"}/>{" "}
                             <a rel="noopener noreferrer" target="_blank"
                                href={`https://www.google.hu/maps/place/${actualLocation["address"]}`}>
-                                 {actualLocation["address"]}
+                                {actualLocation["address"]}
                             </a>
                         </h3>
-                        <h3>Név: {actualLocation["contactName"]}</h3>
-                        <h3>Tel: {actualLocation["contactPhone"]}</h3>
-                        <h3>Mail: {actualLocation["contactMail"]}</h3>
+                        <h3><PersonIcon className={"icon"}/>{" "} {actualLocation["contactName"]}</h3>
+                        <h3><PhoneAndroidTwoToneIcon className={"icon"}/>{" "} {actualLocation["contactPhone"]}</h3>
+                        <h3><MailIcon className={"icon"}/>{" "} {actualLocation["contactMail"]}</h3>
                     </div>
+                    {hasRole(["admin"]) &&
+                    <EditContactsModal contacts={actualLocation} locationName={locationName.split("_").join(" ")}/>
+                    }
                 </div>
             </ContactStyle>
         )
